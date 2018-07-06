3daa0929bf7807bc5031b25ddc73a20e39ab4086
ASemina <34461262+ASemina@users.noreply.github.com>
2018-07-05 22:48:34 -0400

    Fix Bug

--- bot.py
+++ bot.py
@@ -232,7 +232,7 @@ def read_messages(request_params, group_id):
     return message_list
 
 #Just slap this shit in here
-def timer(rest, user_id, message):
+def start_timer(rest, user_id, message):
     global i
     global timer
     timer = (True, i + (60 * int(rest[2])), user_id, message)
@@ -278,9 +278,9 @@ def commands(message_list, udict):
             elif text.lower().startswith('timer'):
                 if (message.attachments != [] and message.attachments[0]['type'] == 'mentions'):
                     name = message.attachments[0]['loci'][0]
-                    rest = text[name[0] + name[1]-1:].strip().split(" ")
+                    rest = text[name[0] + name[1]:].strip().split(" ")
                     if (len(rest) == 1 and rest[0].isdigit()):
-                        timer(rest, message.attachments[0]['user_ids'][0], message)
+                        start_timer(rest, message.attachments[0]['user_ids'][0], message)
                         post_params['text'] = 'Timer set for ' + rest[1] + 'minutes'
                         send_message(post_params)
                     else: 
