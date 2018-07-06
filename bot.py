caf79eeb213cc30978cfbaee3b9a7b94a32bddb3
ASemina <34461262+ASemina@users.noreply.github.com>
2018-07-05 22:35:31 -0400

    Fix Bug

--- bot.py
+++ bot.py
@@ -284,7 +284,7 @@ def commands(message_list, udict):
                         post_params['text'] = 'Timer set for ' + rest[1] + 'minutes'
                         send_message(post_params)
                     else: 
-                        post_params['text'] = "I don't know when that is" + str(rest)
+                        post_params['text'] = "I don't know when that is" + str(text)
                         send_message(post_params)
                 else:
                     post_params['text'] = "I don't know who that is"
