e31f86517a3611af98b437a2dd045117f75d7a4f
bryanalexander13 <40750998+bryanalexander13@users.noreply.github.com>
2018-07-05 20:16:20 -0400

    upload bot

---
+++ bot.py
@@ -0,0 +1,375 @@
+import requests
+import time
+import json
+import pandas
+import os
+
+
+#request_params = {'token': '7qcn6czO31you5eWWJlZqVFW42Hp1YobbYoP3E3C'}
+#response_messages = requests.get('https://api.groupme.com/v3/groups/41812904/messages', params = request_params).json()['response']['messages']
+#response_messages
+class User:
+    """Users information"""
+    def __init__(self, user_id, name):
+        self.user_id = user_id
+        self.name = name
+        self.realness = 0
+
+    def add_realness(self):
+        self.realness += 1
+
+    def subtract_realness(self):
+        self.realness -= 1
+
+
+class Message:
+    """A message handler"""
+
+    def __init__(self,
+                    attachments,
+                    avatar_url,
+                    created_at,
+                    favorited_by,
+                    group_id,
+                    id,
+                    name,
+                    sender_id,
+                    sender_type,
+                    source_guid,
+                    system,
+                    text,
+                    user_id):
+        self.attachments = attachments
+        self.avatar_url = avatar_url
+        self.created_at = created_at
+        self.liked = favorited_by
+        self.group_id = group_id
+        self.id = id
+        self.name = name
+        self.sender_id = sender_id
+        self.sender_type = sender_type
+        self.source_guid = source_guid
+        self.system = system
+        self.text = text
+        self.user_id = user_id
+
+    def count_likes(self):
+        return len(self.liked)
+
+
+#loads token and group_id
+def auth_load():
+    with open(os.path.abspath('auth.json'),'r') as auth:
+        file = auth.readlines()
+        data = json.loads(file[0])
+        return data['token'], data['group_id'], data['bot_id']
+
+#loads user information
+def user_load():
+    with open(os.path.abspath('users.json'),'r') as users:
+        file = users.readlines()
+        data = json.loads(file[0])
+        return data
+
+#update userdictionary, realness, nicknames
+def update_users(udict):
+    with open(os.path.abspath('users.json'),'w') as users:
+        users.write(json.dumps(udict))
+
+#loads last message checked
+def last_load():
+    with open(os.path.abspath('last.json'),'r') as last:
+        file = last.readlines()
+        data = json.loads(file[0])
+        return data['last_read']
+
+#writes json file with last message id
+def last_write(last_message):
+    if int(last_message) > int(last_load()):
+        with open(os.path.abspath('last.json'),'w') as l:
+            l.write(json.dumps({'last_read':last_message}))
+
+#sends a message
+def send_message(post_params):
+    requests.post("https://api.groupme.com/v3/bots/post", params = post_params)
+
+#looks up user_id by name
+def id_lookup(name,udict):
+    namelist = []
+    for id, information in udict.items():
+        namelist.append(information['name'])
+    if name not in namelist:
+        return 'no id'
+    for id, information in udict.items():
+        if information['name'] == name.lower():
+            return id
+
+def nickname_instead_of_name(udict_id_key):
+    if len(udict_id_key['name']) == 0:
+        return 'nickname'
+    else:
+        return 'name'
+
+#adds point of realness, updates new dictionary
+def add_realness(names, udict, message):
+    global post_params
+    header = 'Real '
+    text = str()
+    if len(names[0]) == 8:
+        for name in names:
+            if name not in udict.keys():
+                continue
+            elif name == message.sender_id:
+                post_params['text'] = 'You can\'t do that.'
+                send_message(post_params)
+            else:
+                udict[name]['realness'] += 1
+                text += udict[name]['name'].capitalize() + '. '
+    else:
+        for name in names:
+            id = id_lookup(name,udict)
+            if id == 'no id':
+                post_params['text'] = 'Invalid Name'
+                send_message(post_params)
+            elif id == message.sender_id:
+                post_params['text'] = 'You can\'t do that.'
+                send_message(post_params)
+            else:
+                udict[id]['realness'] += 1
+                text += name.capitalize() + '. '
+    if len(text) > 0:
+        post_params['text'] = header + text
+        send_message(post_params)
+        update_users(udict)
+
+#subtracts point of realness, updates new dictionary
+def subtract_realness(names, udict, message):
+    global post_params
+    terminal = False
+    header = 'Not Real '
+    text = str()
+    if len(names[0]) == 8:
+        for name in names:
+            if name not in udict.keys():
+                continue
+            elif name == message.sender_id:
+                post_params['text'] = 'You can\'t do that.'
+                send_message(post_params)
+            else:
+                udict[name]['realness'] -= 1
+                text += udict[name]['name'].capitalize() + '. '
+                if udict[name]['name'] == 'carter':
+                    terminal = True
+    else:
+        for name in names:
+            id = id_lookup(name,udict)
+            if id == 'no id':
+                post_params['text'] = 'Invalid Name'
+                send_message(post_params)
+            elif id == message.sender_id:
+                post_params['text'] = 'You can\'t do that.'
+                send_message(post_params)
+            else:
+                udict[id]['realness'] -= 1
+                text += name.capitalize() + '. '
+                if name == 'carter':
+                    terminal = True
+    if len(text) > 0:
+        post_params['text'] = header + text
+        send_message(post_params)
+        update_users(udict)
+    if terminal == True:
+        post_params['text'] = 'It is terminal.'
+        send_message(post_params)
+
+
+def realness(udict):
+    global post_params
+    header = 'The current realness levels are:'
+    levels=str()
+    realness_list = []
+    for id in userdict:
+        realness_list.append((udict[id]['realness'],udict[id][nickname_instead_of_name(udict[id])]))
+    realness_list = sorted(realness_list,reverse=True)
+    for k, v in realness_list:
+        levels += '\n' + v.capitalize() +' : '+ str(k)
+    txt = header + levels
+    post_params['text'] = txt
+    send_message(post_params)
+
+#update userdict name
+# def update(sender_id, name, udict):
+#     global post_params
+#     udict[sender_id]['name'] = name.lower()
+#     update_users(udict)
+#     post_params['text'] = 'Updated.'
+#     send_message(post_params)
+
+#reads messages and creates message_list of Message objects
+def read_messages(request_params, group_id):
+    response_messages = requests.get('https://api.groupme.com/v3/groups/{}/messages'.format(group_id), params = request_params).json()['response']['messages']
+    message_list=[]
+    for message in response_messages[::-1]:
+        message_list.append(Message(message['attachments'],
+                                        message['avatar_url'],
+                                        message['created_at'],
+                                        message['favorited_by'],
+                                        message['group_id'],
+                                        message['id'],
+                                        message['name'],
+                                        message['sender_id'],
+                                        message['sender_type'],
+                                        message['source_guid'],
+                                        message['system'],
+                                        message['text'],
+                                        message['user_id']))
+        if message['sender_type'] == 'bot':
+            continue
+        elif message['sender_type'] == 'system':
+            continue
+        elif message['user_id'] not in list(userdict.keys()):
+            userdict[message['user_id']] = {'name':'','nickname': message['name'],'realness' : 0}
+        elif userdict[message['user_id']]['nickname'] != message['name']:
+            userdict[message['user_id']]['nickname'] = message['name']
+    update_users(userdict)
+    return message_list
+
+#read_messages(request_params, group_id)[0].content
+
+#checks for last message and runs commands
+def commands(message_list, udict):
+    global post_params
+    for message in message_list:
+        if int(message.id) <= int(last_load()):
+            continue
+        elif message.text == None:
+            continue
+        elif message.sender_type == 'bot':
+            last_write(message.id)
+        elif message.text.lower().startswith('@rb'):
+            last_write(message.id)
+            text = message.text.split('@rb ')[1]
+            if text.lower().startswith('very real'):
+                if len(text.split('very real')[1]) == 0:
+                    post_params['text'] = 'Nothing to add realness to.'
+                    send_message(post_params)
+                elif len(message.attachments) > 0:
+                    add_realness(message.attachments[0]['user_ids'], udict, message)
+                else:
+                    modtextlist = text.lower().split()
+                    modtextlist.remove('very')
+                    modtextlist.remove('real')
+                    add_realness(modtextlist, udict, message)
+            elif text.lower().startswith('not real'):
+                if len(message.attachments) > 0:
+                    subtract_realness(message.attachments[0]['user_ids'], udict, message)
+                elif len(text.split('not real')[1]) == 0:
+                    post_params['text'] = 'Nothing to add realness to.'
+                    send_message(post_params)
+                else:
+                    modtextlist = text.lower().split()
+                    modtextlist.remove('not')
+                    modtextlist.remove('real')
+                    subtract_realness(modtextlist, udict, message)
+            elif text == 'rankings':
+                realness(udict)
+            elif text.lower().startswith('update'):
+                if len(text.split('update')) == 1:
+                    post_params['text'] = 'Nothing to update.'
+                    send_message(post_params)
+                #else:
+                    #update(message.sender_id,text.split('update')[1],udict)
+
+def run():
+    global request_params
+    global group_id
+    global userdict
+    #i = 0
+    while (2+2 == 4):
+        message_list = read_messages(request_params, group_id)
+        commands(message_list, userdict)
+        #i += 1
+        time.sleep(1)
+
+if __name__ == "__main__":
+    userdict = user_load()
+    auth = auth_load()
+    group_id = auth[1]
+    request_params = {'token':auth[0]}
+    post_params = {'text':'','bot_id':auth[2],'attachments':[]}
+
+    print('The current realness levels are: ')
+    for k, v in userdict.items():
+        print('{} : {}'.format(v['name'],v['realness']))
+    run()
+
+
+# def get_realness(udict_list):
+#     for x in udict_list:
+#         x[]
+# for k, v in sorted(list(userdict.items()), key = userdict[k]['realness']):
+#     print(userdict[k]['name'] + userdict[k]['realness'])
+#
+# adict = {'a':5,'b':3,'c':4}
+# list(adict.items())
+# def takeSecond(elem):
+#     return elem[1]
+#
+# realness_list=[]
+# for id in userdict:
+#     realness_list.append((userdict[id]['realness'],userdict[id]['name']))
+# for k in sorted(realness_list,k[1]):
+#     print(k+':'+str(k[1]))
+# realness_list
+#
+# sortedlist=sorted(realness_list, reverse=True)
+# stra=str()
+# for k, v in sortedlist:
+#     stra += v+' : '+str(k)
+# stra
+#
+#
+# userdict
+#
+# userdict['22776072']['realness']=5
+#
+# for k, v in list(userdict.items()):
+#     print(v['name']+str(v['realness']))
+# message_list=[]
+# response_messages
+# message_list[0].attachments[0]
+# len(message_list[1].attachments)
+# if 'type' in message_list[1].attachments[0].keys():
+#     print('true')
+# message_list[5].attachments['type']
+#
+#
+# texta = '@rb very real BRYAN'
+# textb = texta.split('@rb ')[1]
+# modtextlist = textb.lower().split()
+# modtextlist
+# modtextlist.remove('very')
+# modtextlist
+# for message in response_messages:
+#     message_list.append(Message(message['attachments'],
+#                                     message['avatar_url'],
+#                                     message['created_at'],
+#                                     message['favorited_by'],
+#                                     message['group_id'],
+#                                     message['id'],
+#                                     message['name'],
+#                                     message['sender_id'],
+#                                     message['sender_type'],
+#                                     message['source_guid'],
+#                                     message['system'],
+#                                     message['text'],
+#                                     message['user_id']))
+#     if message['user_id'] not in list(userdict.keys()):
+#         userdict[message['user_id']] = {'name':'','nickname': message['name'],'realness' : 0}
+#     if userdict[message['user_id']]['nickname'] != message['name']:
+#         userdict[message['user_id']]['nickname'] = message['name']
+#
+#
+#
+#
+#
